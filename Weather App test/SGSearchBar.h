82c2c6a5fcf02086cfa990ca3245dd44dea8f927
leapfrog-saugat <saugatgautam@lftechnology.com>
2015-03-11 12:57:42 +0545

    Committing all the changes in WeatherData.h/WeatherData.m and SGSearchBar.h/SGSearchBar.m

---
+++ Weather App test/SGSearchBar.h
@@ -0,0 +1,13 @@
+//
+//  SGSearchBar.h
+//  Weather App test
+//
+//  Created by Saugat Gautam on 3/11/15.
+//  Copyright (c) 2015 Saugat Gautam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGSearchBar : UIView
+
+@end
---
+++ Weather App test/SGSearchBar.m
@@ -0,0 +1,21 @@
+//
+//  SGSearchBar.m
+//  Weather App test
+//
+//  Created by Saugat Gautam on 3/11/15.
+//  Copyright (c) 2015 Saugat Gautam. All rights reserved.
+//
+
+#import "SGSearchBar.h"
+
+@implementation SGSearchBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
